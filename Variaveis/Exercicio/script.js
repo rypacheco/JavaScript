f35a78340e918b0f89ba76da29fb682ce852059a
Ryan Pacheco <ryanaraujoxd@gmail.com>
2025-08-23 17:24:26 -0300

    adicionando um exercicio para fixar o conteudo sobre variaveis, usando const, let, var e variavel sem valor definido.

---
+++ Variaveis/Exercicio/script.js
@@ -0,0 +1,18 @@
+// Exercício
+
+// Declarar uma variável com o seu nome
+const name = 'Ryan';
+// Declarar uma variável com a sua idade
+const age = 24
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+let favoriteFood;
+// Atribuir valor a sua comida favorita
+favoriteFood = 'Pizza';
+// Declarar 5 variáveis diferentes sem valores
+var favoriteGame, 
+    favoriteMovie,
+    favoriteMusic,
+    favoriteDrink,
+    favoriteTeam;
+
+console.log(name,age,favoriteFood);
