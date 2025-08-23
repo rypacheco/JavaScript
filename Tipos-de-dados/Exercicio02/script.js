04433f44358cda061eb900622522673cbddde707
Ryan Pacheco <ryanaraujoxd@gmail.com>
2025-08-23 19:52:53 -0300

    feito um exercicio para colocar em pratica o que aprendi na aula de tipo de dados.

---
+++ Tipos-de-dados/Exercicio02/script.js
@@ -0,0 +1,14 @@
+// Declare uma variável contendo uma string
+var catName = 'Lili';
+// Declare uma variável contendo um número dentro de uma string
+var stringNumber = '30';
+// Declare uma variável com a sua idade
+var age = 23;
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var name = 'Hau';
+var lastName = 'ziinho';
+var fullName = name + lastName;
+// Coloque a seguinte frase em uma variável: It's time
+var word1 = `it's mine ${catName}`;
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof name);
