2f858462969c1afd03bb0c8127aa7468a7ad3c52
Ryan Pacheco <ryanaraujoxd@gmail.com>
2025-08-23 22:02:37 -0300

    exercicio para patricar o que aprendi com numeros e operadores.

---
+++ Numeros-e-operadores/Exercicio-numeros-e-operadores/script.js
@@ -0,0 +1,25 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // 2* 5 = 10/2 = 5 + 20 = 25 + 10 = 35
+
+
+// Crie duas expressões que retornem NaN
+var expressao1 = '20e' / 2;
+var express2 = 30 - 'teste';
+console.log(expressao1, express2);
+
+
+
+// Somar a string '200' com o número 50 e retornar 250
+var valor1 = +'200' + 50;
+console.log(soma)
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var incremento = 5
+console.log(++incremento);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(pesoPorDois)
